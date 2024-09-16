0947df4871428dbcdfc64e5a5ac8798cab6e1755
morriswa <william@morriswa.org>
2024-09-16 12:41:41 -0500

    breakout project structure for readability

---
+++ src/models.py
@@ -0,0 +1,77 @@
+import settings
+
+class Player:  # stores the data for each player so we can alternate easily with each turn based on the Player data
+    def __init__(self, num):  # (A) initialization
+        self.num = num  # (A) player num to keep track of who's who  without anything fancy
+        self.board = [[0 for _ in range(settings.COLS)] for _ in
+                      range(settings.ROWS)]  # (A) initialize player's matrix based on game board to reflect ship states
+        self.guesses = [[0 for _ in range(settings.COLS)] for _ in
+                        range(settings.ROWS)]  # (A) similarly, a matrix to reflect guesses on the enemy that are accurate/misses
+        self.ships = {}  # (N) a dict for the ships that will hold the size of the ships and the number of times the ship has been hit
+        self.sunk_ships = {}  # (N) dict that holds the ships that are sunk that belong to the player themselves
+
+    def place_ship(self, x, y, size, direction):  # (A) return if valid placement based on player's board
+        if direction == 0:  # (A) refers to the current direction being used in the start screen that was passed through (0 is to the right)
+            if x + size > settings.COLS:  # (A) too large for the board
+                return False  # (A) not a valid move
+            for i in range(
+                    size):  # (A) now if not too large, we must calculate if there are any collisions with other placements
+                if self.board[y][x + i] != 0:  # (A) if not 0 so not free in the path of placement...
+                    return False  # (A) then this is not a valid move
+            for i in range(size):  # (A) otherwise, replace the path with ships of size
+                self.board[y][
+                    x + i] = size  # (A) important to set nodes in the path to 'size' because that will distinguish different ships
+        elif direction == 1:  # (A) similarly, refer to the above; direction == 1 means it is a path pointing down
+            if y + size > settings.ROWS:
+                return False
+            for i in range(size):
+                if self.board[y + i][x] != 0:
+                    return False
+            for i in range(size):
+                self.board[y + i][x] = size
+        elif direction == 2:  # (A) refer to above, direction == 2 means it is a path pointing to the left
+            if x - size + 1 < 0:  # (A) distinction between this conditinoal and the above is that 0 can be valid, but ROWS/COLS cannot hence the + 1
+                return False
+            for i in range(size):
+                if self.board[y][x - i] != 0:
+                    return False
+            for i in range(size):
+                self.board[y][x - i] = size
+        elif direction == 3:  # (A) refer to above, direction == 3 means it is a path pointing to the top
+            if y - size + 1 < 0:  # (A) same explanation as direction == 2 for this conditional
+                return False
+            for i in range(size):
+                if self.board[y - i][x] != 0:
+                    return False
+            for i in range(size):
+                self.board[y - i][x] = size
+
+        if size not in self.ships:  # (N) initializing the dicts for the self.ships and self.sunk_ships for each ship size on the board. They are initialized to 0 and False respectively. Adapted from ChatGPT but changed to fit the logic and fix some errors
+            self.ships[size] = 0
+            self.sunk_ships[size] = False
+        return True
+
+    def check_hit(self, enemy, x,
+                  y):  # (N) function that checks for a hit on an enemy ship. Takes in the parameters the enemy player object as well as the x and y coordinates on the board that was fired at. Partially taken from ChatGPT but mostly changed to fix issues with the function
+        if enemy.board[y][x] > 0:  # (N) checking to see if the size of a ship is marked on that space of the board
+            ship_size = enemy.board[y][x]  # (N) if that is the case assign a variable to be the size of that ship
+            self.guesses[y][x] = 'hit'  # (N) add to the player guesses that the coordinate was a hit
+            enemy.ships[ship_size] += 1  # (N) add a hit count to the specific ship of the size that was hit
+            if enemy.ships[ship_size] == ship_size and not enemy.sunk_ships[
+                ship_size]:  # (N) if the # of hits are the same as the size of the ship, and it is not already sunk
+                enemy.mark_ship_as_sunk(self,
+                                        ship_size)  # (N) mark the ship as sunk in the enemy's dict of sunk ships (meaning that the enemy keeps track of which of their ships are sunk not the current player)
+            return True  # (N) return that the shot was a hit
+        else:
+            self.guesses[y][x] = 'miss'  # (N) or the shot is a miss and return that it is a miss
+            return False
+
+    def mark_ship_as_sunk(self, currentPlayer,
+                          ship_size):  # (N) function that will mark a ship as sunk in the enemy's dict of their own ships. Initially adapted from ChatGPT but almost fully changed to fit the program logic
+        self.sunk_ships[
+            ship_size] = True  # (N) mark in your own dict of sunken ships (in this case the enemy) that the ship of that size is sunk
+        for y in range(settings.ROWS):  # (N) iterate through all cols and rows
+            for x in range(settings.COLS):
+                if self.board[y][
+                    x] == ship_size:  # (N) if any spaces correspond to the size of the ship that was sunk, mark them as sunk in the player's guesses, not the enemy's
+                    currentPlayer.guesses[y][x] = 'sunk'
---
+++ src/settings.py
@@ -0,0 +1,6 @@
+FPS = 30 # (A) global var to determine refresh rate of the game
+ROWS, COLS = 10, 10 # (A) how many blocks in the rows and cols will be used to calculate positionings (10x10 is standard battleship)
+BLOCKHEIGHT, BLOCKWIDTH = 30, 30 # (A) height of each block for the board
+GAMEHEIGHT, GAMEWIDTH = 700, 600 # (A) height/width of the actual game window
+
+SHIPCOLORS = {1: (255, 100, 100), 2: (100, 255, 100), 3: (100, 100, 255), 4: (255, 255, 100), 5: (255, 100, 255)} # (A) global colors for different type of ships
