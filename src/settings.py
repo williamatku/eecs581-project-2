c538521b4f184270320bc23880283d872f1e0173
Kevin <kevin.rivers14832@ku.edu>
2024-09-20 16:41:41 -0500

    Adding other images for ship sizes.

--- src/settings.py
+++ src/settings.py
@@ -6,9 +6,9 @@ BLOCKHEIGHT, BLOCKWIDTH = 30, 30 # (A) height of each block for the board
 GAMEHEIGHT, GAMEWIDTH = 700, 600 # (A) height/width of the actual game window
 
 SHIP1 = pygame.image.load('images/shipImages/shipSize1.png')
-SHIP2 = pygame.image.load('images/shipImages/shipSize1.png')
-SHIP3 = pygame.image.load('images/shipImages/shipSize1.png')
-SHIP4 = pygame.image.load('images/shipImages/shipSize1.png')
-SHIP5 = pygame.image.load('images/shipImages/shipSize1.png')
+SHIP2 = pygame.image.load('images/shipImages/shipSize2.png')
+SHIP3 = pygame.image.load('images/shipImages/shipSize3.png')
+SHIP4 = pygame.image.load('images/shipImages/shipSize4.png')
+SHIP5 = pygame.image.load('images/shipImages/shipSize5.png')
 
 SHIPCOLORS = {1: SHIP1, 2: SHIP2, 3: SHIP3, 4: SHIP4, 5: SHIP5} # (A) global colors for different type of ships
