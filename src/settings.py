a59c68e9c777a006c471e03e042e6383736885b4
morriswa <william@morriswa.org>
2024-09-24 09:46:25 -0500

    PROJECT start setting up static content

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+
+
 FPS = 30 # (A) global var to determine refresh rate of the game
 ROWS, COLS = 10, 10 # (A) how many blocks in the rows and cols will be used to calculate positionings (10x10 is standard battleship)
 BLOCKHEIGHT, BLOCKWIDTH = 30, 30 # (A) height of each block for the board
@@ -5,3 +7,11 @@ GAMEHEIGHT, GAMEWIDTH = 700, 600 # (A) height/width of the actual game window
 TURN_TIME_OUT_SECONDS = 3
 
 SHIPCOLORS = {1: (255, 100, 100), 2: (100, 255, 100), 3: (100, 100, 255), 4: (255, 255, 100), 5: (255, 100, 255)} # (A) global colors for different type of ships
+
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+IMAGES = BASE_DIR.joinpath('./static/images')
+SOUNDS = BASE_DIR.joinpath('./static/sounds')
+
+def SOUND(sound_file: str):
+    return SOUNDS.joinpath(f'./{sound_file}')
