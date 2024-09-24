31a6658734775562e8df84117d7f46ec28bbbbb5
morriswa <william@morriswa.org>
2024-09-24 12:22:04 -0500

    PROJECT # streamline start_game()

--- src/game.py
+++ src/game.py
@@ -18,53 +18,97 @@ from views import showStartMenu, showGameView, showAIModeSelection, showTurnTran
 from models import Player
 
 
+def pvp(screen, ship_count):
+
+    clock = pygame.time.Clock() # (A) clock that keeps track of how many times the screen is updated
+
+    playerOne = Player(1)  # (A) initialize playerOne, with a Player(num)-- num marker of 1 to differentiate
+    playerTwo = Player(2)  # (A) playertwo with player.num = 2
+    # print(playerOne.board)
+
+    currentPlayer = playerOne  # (A) game will start with playerOne, so currentPlayer is initialized
+    enemy = playerTwo  # (A) enemy for now is playerTwo, but these roles will be swapped every game loop
+
+    game = True  # (A) game conditional loop
+    setUp = True  # (A) check that'll only run the startBoard() once for ships
+
+    while game:  # (A) while the game is running
+        drawBackground(screen)
+        if setUp:  # (A) conditional met with first time run of the loop
+            showGameView(screen, ship_count, playerOne)  # (A) create the matrix for playerOne with ship selection
+            showTurnTransitionScreen(screen, '2')
+            showGameView(screen, ship_count, playerTwo)  # (A) do the same for playerTwo
+            showTurnTransitionScreen(screen, '1')
+            setUp = False  # (A) set condition to false, won't run again for remainder of the game
+        else:  # (A) when the boards have been set up
+            font = pygame.font.Font(None, 28)  # (A) font object with no font type and 28 font size
+            turn_text = font.render(f"Player {currentPlayer.num}'s Turn", True, (5, 5, 5))  # (A) render the text
+            screen.blit(turn_text, (
+                settings.GAMEWIDTH // 2 - turn_text.get_width() // 2,
+                350
+            ))  # (A) push the rendered text to the top of the screen, placed horizontal and in the middle vertically
+
+            # (A) handle the player turn, will swap players (curr/enemy) after each successful playerturn
+            game, currentPlayer, enemy = handlePlayerTurn(screen, currentPlayer, enemy)
+            if game:  # (A) if the game is still going on... may be a redundant conditional in hindsight
+                showTurnTransitionScreen(screen, currentPlayer.num)
+                currentPlayer, enemy = enemy, currentPlayer  # (A) then swap the two players
+
+        pygame.display.flip()  # (A) flip to update the display as needed
+        for event in pygame.event.get():  # (A) listen to events
+            if event.type == pygame.QUIT:  # (A) if user exits out
+                game = False  # (A) game is over
+        clock.tick(settings.FPS)  # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
+
+
+def pvc_hard(screen, count):
+
+    clock = pygame.time.Clock() # (A) clock that keeps track of how many times the screen is updated
+
+    print("You chose hard mode!")
+    playerOne = Player(1)  # (A) initialize playerOne, with a Player(num)-- num marker of 1 to differentiate
+    drawBackground(screen)
+
+    # player picks their ships using startBoard
+    showGameView(screen, count, playerOne)  # (A) create the matrix for playerOne (only player)
+
+    # AI gets matrix that says where all ships are
+    cheating_board = playerOne.board
+    print(cheating_board)
+
+    while game:
+        drawBackground(screen)
+        font = pygame.font.Font(None, 28)  # (A) font object with no font type and 28 font size
+        turn_text = font.render(f"Player {playerOne.num}'s Turn", True, (5, 5, 5))  # (A) render the text
+        screen.blit(turn_text, (settings.GAMEWIDTH // 2 - turn_text.get_width() // 2,
+                                350))  # (A) push the rendered text to the top of the screen, placed horizontal and in the middle vertically
+        game, currentPlayer, enemy = handlePlayerTurn(screen, currentPlayer,
+                                                      enemy)  # (A) handle the player turn, will swap players (curr/enemy) after each successful playerturn
+        if game:  # (A) if the game is still going on... may be a redundant conditional in hindsight
+            currentPlayer, enemy = enemy, currentPlayer  # (A) then swap the two players
+
+    pygame.display.flip()  # (A) flip to update the display as needed
+    for event in pygame.event.get():  # (A) listen to events
+        if event.type == pygame.QUIT:  # (A) if user exits out
+            game = False  # (A) game is over
+
+    clock.tick(settings.FPS)  # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
+
+
 def start_game(): # (A) main function that starts the game
     pygame.init() # (A) initialize the pygame engine so it can listen for inputs/handle screens
     pygame.display.set_caption("battleship") # (A) set up the title of the game
-    game = True # (A) game conditional loop
 
     screen = pygame.display.set_mode((settings.GAMEWIDTH, settings.GAMEHEIGHT)) # (A) the main screen that gets passed around, initialized with a display of GAMEWIDTH and GAMEHEIGHT
-    clock = pygame.time.Clock() # (A) clock that keeps track of how many times the screen is updated
 
     count = showStartMenu(screen)  # (A) initial getCount() will be the default starting screen to find how many ships to play with
-    setUp = True  # (A) check that'll only run the startBoard() once for ships
 
     # Call the mode selection screen
     mode = showOpponentSelection(screen)
 
 
     if mode == "Player":  # If the user selected to play against another player
-
-        playerOne = Player(1) # (A) initialize playerOne, with a Player(num)-- num marker of 1 to differentiate
-        playerTwo = Player(2) # (A) playertwo with player.num = 2
-        # print(playerOne.board)
-
-        currentPlayer = playerOne # (A) game will start with playerOne, so currentPlayer is initialized
-        enemy = playerTwo # (A) enemy for now is playerTwo, but these roles will be swapped every game loop
-
-        while game: # (A) while the game is running
-            drawBackground(screen)
-            if setUp: # (A) conditional met with first time run of the loop
-                showGameView(screen, count, playerOne) # (A) create the matrix for playerOne with ship selection
-                showTurnTransitionScreen(screen, '2')
-                showGameView(screen, count, playerTwo) # (A) do the same for playerTwo
-                showTurnTransitionScreen(screen, '1')
-                setUp = False # (A) set condition to false, won't run again for remainder of the game
-            else: # (A) when the boards have been set up
-                font = pygame.font.Font(None, 28) # (A) font object with no font type and 28 font size
-                turn_text = font.render(f"Player {currentPlayer.num}'s Turn", True, (5, 5, 5)) # (A) render the text
-                screen.blit(turn_text, (settings.GAMEWIDTH // 2 - turn_text.get_width() // 2, 350)) # (A) push the rendered text to the top of the screen, placed horizontal and in the middle vertically
-
-                game, currentPlayer, enemy = handlePlayerTurn(screen, currentPlayer, enemy) # (A) handle the player turn, will swap players (curr/enemy) after each successful playerturn
-                if game: # (A) if the game is still going on... may be a redundant conditional in hindsight
-                    showTurnTransitionScreen(screen, currentPlayer.num)
-                    currentPlayer, enemy = enemy, currentPlayer # (A) then swap the two players
-
-            pygame.display.flip() # (A) flip to update the display as needed
-            for event in pygame.event.get(): # (A) listen to events
-                if event.type == pygame.QUIT: # (A) if user exits out
-                    game = False # (A) game is over
-            clock.tick(settings.FPS) # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
+        pvp(screen, count)
 
     elif mode == "AI":  # AI functionality placeholder
 
@@ -75,32 +119,5 @@ def start_game(): # (A) main function that starts the game
             pass
         elif difficulty == "Medium":
             pass
-
         elif difficulty == "Hard":
-            print("You chose hard mode!")
-            playerOne = Player(1) # (A) initialize playerOne, with a Player(num)-- num marker of 1 to differentiate
-            drawBackground(screen)
-
-            # player picks their ships using startBoard
-            showGameView(screen, count, playerOne) # (A) create the matrix for playerOne (only player)
-
-            # AI gets matrix that says where all ships are
-            cheating_board = playerOne.board
-            print(cheating_board)
-
-            while game:
-                drawBackground(screen)
-                font = pygame.font.Font(None, 28) # (A) font object with no font type and 28 font size
-                turn_text = font.render(f"Player {playerOne.num}'s Turn", True, (5, 5, 5)) # (A) render the text
-                screen.blit(turn_text, (settings.GAMEWIDTH // 2 - turn_text.get_width() // 2, 350)) # (A) push the rendered text to the top of the screen, placed horizontal and in the middle vertically
-                game, currentPlayer, enemy = handlePlayerTurn(screen, currentPlayer, enemy) # (A) handle the player turn, will swap players (curr/enemy) after each successful playerturn
-                if game: # (A) if the game is still going on... may be a redundant conditional in hindsight
-                    currentPlayer, enemy = enemy, currentPlayer # (A) then swap the two players
-
-
-
-            pygame.display.flip() # (A) flip to update the display as needed
-            for event in pygame.event.get(): # (A) listen to events
-                if event.type == pygame.QUIT: # (A) if user exits out
-                    game = False # (A) game is over
-            clock.tick(settings.FPS) # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
+            pvc_hard(screen, count)
