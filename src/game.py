cddb91944d8e4f55ebe02fbd0dd133df7f3d3fb3
Timothy Holmes <holmestimo11@gmail.com>
2024-09-29 14:56:11 -0500

    added player vs computer medium mode

--- src/game.py
+++ src/game.py
@@ -105,6 +105,43 @@ def pvc_hard(count):
 
     clock.tick(settings.FPS)  # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
 
+def pvc_medium(count):
+
+    screen = getScreen()
+    clock = pygame.time.Clock() # (A) clock that keeps track of how many times the screen is updated
+
+    logging.info("You chose medium mode!")
+    playerOne = Player(1)  # (A) initialize playerOne, with a Player(num)-- num marker of 1 to differentiate
+    drawBackground()
+
+    # player picks their ships using startBoard
+    showGameView(count, playerOne)  # (A) create the matrix for playerOne (only player)
+
+    game = True  # (A) game conditional loop
+    #setUp = True  # (A) check that'll only run the startBoard() once for ships
+
+    while game:
+        drawBackground()
+        font = pygame.font.Font(None, 28)  # (A) font object with no font type and 28 font size
+        turn_text = createText(f"Player {playerOne.num}'s Turn", {
+            'color': (5, 5, 5),
+            'font-size': getFontSizePx('med'),
+        })  # (A) render the text
+
+        # (A) push the rendered text to the top of the screen, placed horizontal and in the middle vertically
+        screen.blit(turn_text, (settings.GAMEWIDTH // 2 - turn_text.get_width() // 2, 350))
+        # (A) handle the player turn, will swap players (curr/enemy) after each successful playerturn
+        game, currentPlayer, enemy = handlePlayerTurn(currentPlayer, enemy)
+        if game:  # (A) if the game is still going on... may be a redundant conditional in hindsight
+            currentPlayer, enemy = enemy, currentPlayer  # (A) then swap the two players
+
+    pygame.display.flip()  # (A) flip to update the display as needed
+    for event in pygame.event.get():  # (A) listen to events
+        if event.type == pygame.QUIT:  # (A) if user exits out
+            game = False  # (A) game is over
+
+    clock.tick(settings.FPS)  # (A) FPS (initialized at the start of the code) will determine refresh rate for the game
+
 
 def start_game(): # (A) main function that starts the game
     pygame.init() # (A) initialize the pygame engine so it can listen for inputs/handle screens
